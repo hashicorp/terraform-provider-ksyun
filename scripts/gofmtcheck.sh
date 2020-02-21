3771d73b67170a44d33da8fff0fc18956911f418
zhuxuanxuan23 <zhuxuanxuan@kingsoft.com>
2020-02-21 11:13:21 +0800

    modify scripts

--- scripts/gofmtcheck.sh
+++ scripts/gofmtcheck.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Check gofmt
 echo "==> Checking that code complies with gofmt requirements..."
-gofmt_files=$(find . -name '*.go' | grep -v vendor | xargs gofmt -l)
+gofmt_files=$(gofmt -l `find . -name '*.go' | grep -v vendor`)
 if [[ -n ${gofmt_files} ]]; then
     echo 'gofmt needs running on the following files:'
     echo "${gofmt_files}"
